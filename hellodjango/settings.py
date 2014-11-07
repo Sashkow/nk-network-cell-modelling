2b7cbf642c8a7ea0999e0bd33c7aa238d29035df
Sashko <sashko@tato-LIFEBOOK-SH531-GFX.(none)>
2014-11-07 02:27:02 +0200

    whitenoise

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -87,3 +87,5 @@ STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- hellodjango/wsgi.py
+++ hellodjango/wsgi.py
@@ -11,6 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 #from dj_static import Cling
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
