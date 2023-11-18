7836338eca8b44d9fb42c79c96b5f8550bbb8b43
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-11-18 03:56:47 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+apt-get update
+apt-get install -y graphviz
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
